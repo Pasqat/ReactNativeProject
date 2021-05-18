5040208378dd13c5f7c6bb9c313bef5f7a9467f8
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-18 15:20:34 +0200

    add FlatList

--- src/components/typography/text.component.js
+++ src/components/typography/text.component.js
@@ -1,12 +1,12 @@
 import styled from "styled-components/native";
 
-const defaulTextStyles = (theme) => `
-    font-family: ${theme.fonts.body};
-    font-weight: ${theme.fontWeights.regular};
-    color: ${theme.colors.text.primary};
-    flex-wrap: wrap;
-    margin-top: 0px;
-    margin-bottom: 0px;
+const defaultTextStyles = (theme) => `
+  font-family: ${theme.fonts.body};
+  font-weight: ${theme.fontWeights.regular};
+  color: ${theme.colors.text.primary};
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-bottom: 0px;
 `;
 
 const body = (theme) => `
@@ -34,13 +34,17 @@ const label = (theme) => `
 
 const variants = {
   body,
-  caption,
   label,
+  caption,
   error,
   hint,
 };
 
 export const Text = styled.Text`
-  ${({ theme }) => defaulTextStyles(theme)}
+  ${({ theme }) => defaultTextStyles(theme)}
   ${({ variant, theme }) => variants[variant](theme)}
 `;
+
+Text.defaultProps = {
+  variant: "body",
+};
--- src/features/restourants/screens/restourant.screen.js
+++ src/features/restourants/screens/restourant.screen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { FlatList, SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { RestourantInfoCard } from "../components/restourant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -14,19 +15,41 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestourantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
 export const RestourantScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestourantListContainer>
-      <RestourantInfoCard />
-    </RestourantListContainer>
+    <FlatList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+        { name: 15 },
+        { name: 16 },
+        { name: 17 },
+        { name: 18 },
+        { name: 19 },
+        { name: 20 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestourantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{ padding: 16 }}
+    />
   </SafeArea>
 );
