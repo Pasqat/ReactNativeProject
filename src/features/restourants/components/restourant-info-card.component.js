a87b8a41be5e96ec9a7c8633e8ed66ae001c1ccb
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-18 10:51:54 +0200

    Finihing the templating of RestourantInfoCard

--- src/features/restourants/components/restourant-info-card.component.js
+++ src/features/restourants/components/restourant-info-card.component.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { SvgXml } from "react-native-svg";
 import Star from "../../../../assets/star";
+import Open from "../../../../assets/open";
 
 const RestourantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -35,17 +37,28 @@ const Rating = styled.View`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
 export const RestourantInfoCard = ({ restourant = {} }) => {
   const {
     name = "Some Restourant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photo = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarly,
+    isClosedTemporarly = true,
   } = restourant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -55,11 +68,24 @@ export const RestourantInfoCard = ({ restourant = {} }) => {
       <RestourantCover key={name} source={{ uri: photo[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map((_, index) => (
-            <SvgXml key={index} xml={Star} width={20} height={20} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, index) => (
+              <SvgXml key={index} xml={Star} width={20} height={20} />
+            ))}
+            <SectionEnd>
+              {isClosedTemporarly && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARLY
+                </Text>
+              )}
+              <View style={{ paddingLeft: 16 }} />
+              {isOpenNow && <SvgXml xml={Open} width={20} height={20} />}
+              <View style={{ paddingLeft: 16 }} />
+              <Image style={{ width: 16, height: 16 }} source={{ uri: icon }} />
+            </SectionEnd>
+          </Rating>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestourantCard>
