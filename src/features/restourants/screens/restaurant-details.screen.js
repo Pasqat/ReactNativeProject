8bb8c1f578144dd2cfddc3a08f1ad3e1e935cba1
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-21 10:18:32 +0200

    add list to restourants-details

--- src/features/restourants/screens/restaurant-details.screen.js
+++ src/features/restourants/screens/restaurant-details.screen.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { List } from "react-native-paper";
 import { SafeArea } from "../../../components/utility/safeArea";
 import { RestourantInfoCard } from "../components/restourant-info-card.component";
 
+// breakfast, launch, dinner, drinks
+
 export const RestaurantDetailScreen = ({ route }) => {
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
+
   const { restaurant } = route.params;
+
   return (
     <SafeArea>
       <RestourantInfoCard restourant={restaurant} />
+      <List.Section title="Menu">
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="food-croissant" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="food" />}
+          expanded={lunchExpanded}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
+        >
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+          expanded={dinnerExpanded}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="glass-cocktail" />}
+          expanded={drinksExpanded}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+      </List.Section>
     </SafeArea>
   );
 };
