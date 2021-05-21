bec7e1439d1eccf12383fcfdbf7678871b3484fd
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-21 11:19:51 +0200

    gestureEnabled on Stack.screen

--- src/infrastructure/navigation/restaurant.navigator.js
+++ src/infrastructure/navigation/restaurant.navigator.js
@@ -19,6 +19,7 @@ export const RestaurantNavigator = () => {
       <RestaurantStack.Screen
         name="RestaurantDetails"
         component={RestaurantDetailScreen}
+        options={{ gestureEnabled: true }}
       />
     </RestaurantStack.Navigator>
   );
