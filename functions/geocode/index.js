9b852bc3e62d00087c393dd1171202f293a3aa3f
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-31 15:19:17 +0200

    adding Places API

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,26 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
 
-  response.json(locationMock);
+  if (mock === "test") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        api: "your API key",
+      },
+      timeout: 1000, // after 1s if no response throw error
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      return response.status(400).send(e.response.data.error_message);
+    });
 };
