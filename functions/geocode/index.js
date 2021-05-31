9381937ae111b05c6f2e08fce6b7e0ab417ce27a
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-05-31 17:32:23 +0200

    add env var to firebase:functions

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,10 +1,11 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
 module.exports.geocodeRequest = (request, response, client) => {
   const { city, mock } = url.parse(request.url, true).query;
 
-  if (mock === "test") {
+  if (mock === "true") {
     const locationMock = locationsMock[city.toLowerCase()];
     return response.json(locationMock);
   }
@@ -13,7 +14,7 @@ module.exports.geocodeRequest = (request, response, client) => {
     .geocode({
       params: {
         address: city,
-        api: "your API key",
+        key: functions.config().google.key,
       },
       timeout: 1000, // after 1s if no response throw error
     })
